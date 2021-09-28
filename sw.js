3a259555e50bf9beb8b930594f2c1fb517c5f3c3
tjolin-awe <43389155+tjolin-awe@users.noreply.github.com>
2021-09-27 21:52:13 -0400

    Added PWA components

--- sw.js
+++ sw.js
@@ -168,6 +168,34 @@ self.addEventListener('install', function(event) {
   );
 });
 
+self.addEventListener('fetch', (event) => {
+  console.log('sw fetch');
+  console.log(event.request.url);
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    }).catch(function (error) {
+      console.log(error);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('sw activate');
+  event.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          console.log('sw removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
+
+
+
 /*
 self.addEventListener('install', function(e) {
   e.waitUntil(
